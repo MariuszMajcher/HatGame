334b28a675a3ad04a0bf31f013768819a227bde9
Mariusz Majcher <Majchermx@gmail.com>
2023-03-29 06:50:05 +0100

    Done some more work now moving on to codecademy

--- main.js
+++ main.js
@@ -23,46 +23,58 @@ class Field {
     }
 
     generateField(x, y, percentage) {
-        if(typeof x !== 'number' || typeof y !== 'number' || (typeof percentage !== 'number' || percentage > 80 || percentage < 5 )) {
-            console.log('Use only numbers please, percentage is only the numbers between 5 and 80!')
+        if(typeof x !== 'number' || typeof y !== 'number' || (typeof percentage !== 'number' || percentage > 50 || percentage < 5 )) {
+            console.log('Use only numbers please, percentage is only the numbers between 5 and 50!')
             return 
         }
         /*DEFINE HOT MANY HOLES THERE WILL BE*/
         let howManyHoles = Math.floor(x * y / 100 * percentage)
-        console.log(howManyHoles)
 
         /*DEFINE RANDOM STARTING POINT*/ 
         let startingPointX = Math.floor(Math.random() * x) 
         let startingPointY = Math.floor(Math.random() * y) 
         let startingPoint = [startingPointX, startingPointY]
 
+        /*DEFINE RANDOM STARTING POINT OF THE HAT*/
+        let hatStartingPointX = Math.floor(Math.random() * x) 
+        let hatStartingPointY = Math.floor(Math.random() * y) 
+        let hatStartingPoint = [hatStartingPointX, hatStartingPointY]
+
         /* GENERATE NEW EMPTY FIELD*/
         let newLine = []
         let newField = []
-        for(let i = 0; i < x; i++) {
-            newField.push(newLine)
-        }
+        
        
 
         for(let i = 0; i < x; i++) {
             for(let j = 0; j < y; j++) {
                 /*CREATE A CHECK AND RANDOMLY CHOOSE WHAT TO FILL THE CELL WITH*/
-                // if(i === startingPointX)
-                newField[i][j] = howManyHoles
+                if(i === startingPointX && j === startingPointY ) {
+                    newLine[j] = pathCharacter
+                } else if(i === hatStartingPointX && j === hatStartingPointY) {
+                    newLine[j] = hat
+                } else {
+                    let holeOrNot = Math.random()
+                    if(holeOrNot > 0.5 && howManyHoles > 0) {
+                        newLine[j] = hole
+                        howManyHoles -= 1
+                    } else {
+                        newLine[j] = fieldCharacter
+                    }
+                }
                 
-                howManyHoles -= 1
+
             }
+                newField.push(newLine)
+                newLine = []
            
         }
-        console.log(newField)
+      
     }
     
 }
 
-const tr = new Field([  
-        ['*', '░', 'O'],
-        ['░', 'O', '░'],
-        ['░', '^', '░'],
-        ])
+const tr = new Field()
 
-tr.generateField(4,6, 70)
+tr.generateField(4,6, 49)
+tr.print()
